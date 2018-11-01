c495c8f5c200b84efb33e20752bec41eca4221fa
chuckcoughlin <chuck_coughlin@comcast.net>
2018-10-31 19:49:08 -0600

    Prior to Gradle.

---
+++ workspace/Joint/src/chuckcoughlin/bert/joint/Skeleton.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2018. Charles Coughlin. All Rights Reserved.
+ * MIT License.
+ */
+package chuckcoughlin.bert.joint;
+public class Skeleton {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
