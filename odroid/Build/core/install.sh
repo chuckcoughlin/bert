dd5b61d734cfa78f5d486ce038fd62c828af4b05
Chuck Coughlin <ccoughlin@ils-automation.com>
2018-11-03 14:59:58 -0600

    Added first gradle build of C++

---
+++ odroid/Build/core/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Working directory is the project directory.
+# Make current directory the project proxy area.
+# Script can be executed either from the top level or proxy directory
+export PATH=$PATH:/usr/local/bin
+if [ -d core ]
+then
+	cd core
+fi
+gradle -i clean assemble install
