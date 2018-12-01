10a4c15efcf806caab11a3f055a8a325d34b9888
Chuck Coughlin <ccoughlin@ils-automation.com>
2018-12-01 14:36:18 -0700

    Cross-compiles the C++ code.

--- odroid/Build/core/install.sh
+++ odroid/Build/core/install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 # Working directory is the project directory.
-# Make current directory the project proxy area.
-# Script can be executed either from the top level or proxy directory
+# The important step here is wnen we change the execution path
+# to reference the arm compilation tools. 
 export PATH=$PATH:/usr/local/bin
 if [ -d core ]
 then
 	cd core
 fi
-gradle -i --scan clean assemble install
+
+
+MAKEFILE="`pwd`/Makefile"
+export GCC_DIR="${HOME}/opt/gcc-arm-none-eabi-7-2018-q2"
+export PATH="$GCC_DIR}/arm-none-eabi/bin:${PATH}"
+export PROJECT_ROOT="../../Core"
+export BUILD_DIR="`pwd`/build"
+
+mkdir -p ${BUILD_DIR}
+cd ${PROJECT_ROOT}
+make -e -f ${MAKEFILE} clean
+make -e -f ${MAKEFILE}
+make -e -f ${MAKEFILE} install
