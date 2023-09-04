4aaa51c1926045d3d5f6ecfc20ed432f3e8ba81c
Chuck Coughlin <ccoughlin@ils-automation.com>
2023-09-04 13:38:02 -0600

    Gradle cleanup for syntax module

--- Bert/syntax/build.gradle.kts
+++ Bert/syntax/build.gradle.kts
@@ -21,36 +21,7 @@ dependencies {
 tasks.generateGrammarSource {
     maxHeapSize = "64m"
     arguments = arguments + listOf("-visitor", "-long-messages", "-package", "chuckcoughlin.bert.syntax")
-    // Keep a copy of generated sources
-    /*
-    doLast {
-        println("Copying generated grammar lexer/parser files to main directory.")
-        copy {
-            from("${buildDir}/generated-src/antlr/main")
-            into("generated-src/main/java")
-        }
-    }
-    */
-    finalizedBy(listOf("jar"))
 }
 
-tasks.classes {
 
-}
-// Generates libs/syntax.jar after executing run-configuration bert:syntax[jar]
-tasks.jar {
-    doLast {
-        println("syntax:jar task ....")
-        /*
-        from(listOf("${buildDir}/classes/java/main"))
-        // Put the jar file in a public place
-        copy {
-            from("${buildDir}/libs/syntax.jar")
-            into("${buildDir}/../../libs/syntax.jar")
-        }
-
-         */
-        println("syntax:jar task complete.")
-    }
-}
 
